4064a34eab848281e39cc11dfd5ad30dbbdd9534
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-02 16:21:40 +0200

    added submit handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { nanoid } from 'nanoid'
 
-const Form = ({ setInputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
   const inputTextHandler = (e) => {
     console.log(e.target.value);
     setInputText(e.target.value);
   };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos, { text: inputText, completed: false, id: nanoid(10) }
+    ]);
+    setInputText("");
+  };
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
