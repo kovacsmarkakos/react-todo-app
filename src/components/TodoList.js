abe0d6311c31849a65bc675628bf8c416b285ad2
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-03 18:24:33 +0200

    added delete and complete function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Todo from './Todo'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
 
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map(todo => (
-          <Todo key={todo.id} text={todo.text} />
+          <Todo setTodos={setTodos} todos={todos} todo={todo} key={todo.id} text={todo.text} />
         ))}
       </ul>
     </div>
