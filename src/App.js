79a3c58a5778e6ccb3653a2b8d9d5dac731d108c
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-07 16:09:23 +0200

    todos saves to local storage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([])
 
   useEffect(() => {
+    getLocalTodos()
+  }, [])
+
+  useEffect(() => {
     filterHandler()
+    saveLocalTodos()
   }, [todos, status])
 
   const filterHandler = () => {
@@ -26,6 +31,19 @@ function App() {
         break
     }
   }
+
+  // Save to local storage
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]))
+    } else {
+      setTodos(JSON.parse(localStorage.getItem("todos")))
+    }
+  }
+
   return (
     <div className="App">
       <header>
